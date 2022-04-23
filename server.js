4bbbbac7bffa36cb215f14670b6befe8ac56d2bf
lv <lvjiajia@fofund.com.cn>
2022-04-23 16:58:05 +0800

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http")
+
+const users = [
+    { id: 1, name: "zhangsan" },
+    { id: 2, name: "lisi" },
+]
+
+const server = http.createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    if(req.url === '/api/users') {
+        res.end(JSON.stringify(users))
+    }else{
+        res.end("Not Found!")
+    }
+})
+
+server.listen(3000, () => {
+    console.log("server is running at port 3000!")
+})
