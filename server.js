05dff34eb07879e1860d6ce66c40583909a688b1
lv <lvjiajia@fofund.com.cn>
2022-04-24 11:54:28 +0800

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require("http")
 
 const users = [
-    { id: 1, name: "zhangsan66666" },
-    { id: 2, name: "lisi77777" },
-    { id: 3, name: "wangwu88888" },
-    { id: 4, name: "zhaoliu99999" },
+    { id: 11111, name: "zhangsan66666" },
+    { id: 22222, name: "lisi77777" },
+    { id: 33333, name: "wangwu88888" },
+    { id: 44444, name: "zhaoliu99999" },
 ]
 
 const server = http.createServer((req, res) => {
